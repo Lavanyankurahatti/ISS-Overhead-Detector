4328d75b7420ed567b28edbd8663d04a80e64723
LAVANYA N KURAHATTI <kurahatti2@gmail.com>
2025-12-01 16:42:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,15 +54,3 @@ while True:
 
 
 
-
-
-
-
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
-
-
-
